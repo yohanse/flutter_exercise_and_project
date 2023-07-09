8c231a51aea243c8859c5208c481ef256c9867b9
yohanse <mehabawyohanse793@gmail.com>
2023-07-09 19:50:43 +0300

    flutter

--- telegram clone/lib/data.dart
+++ telegram clone/lib/data.dart
@@ -1,3 +1,7 @@
+// next move change to class that called data
+
+
+
 List<List<dynamic>> userMessage = [
   // First dialog - Person A and Person B
   [
@@ -24,64 +28,64 @@ List<List<dynamic>> userMessage = [
   // Second dialog - Person A and Person B
   [
     ["Hey", false],
-    ["Hi there", false],
+    ["Hi there", true],
     ["What's up?", false],
     ["Not much, just working on my project", false],
-    ["This is Person B's message", false],
-    ["Another message from Person B", false],
+    ["This is Person B's message", true],
+    ["Another message from Person B", true],
     ["Yes, the weather is perfect!", false],
-    ["I went hiking over the weekend.", false],
+    ["I went hiking over the weekend.", true],
     ["That sounds adventurous! How was it?", false],
-    ["It was breathtaking! The views were amazing.", false],
+    ["It was breathtaking! The views were amazing.", true],
     ["I watched that movie too. I loved it!", false],
-    ["That's awesome! Let's plan a movie night soon.", false],
+    ["That's awesome! Let's plan a movie night soon.", true],
     ["Sure, I can't wait!", false],
-    ["The beach sounds relaxing. Enjoy your time there!", false],
+    ["The beach sounds relaxing. Enjoy your time there!", true],
     ["Thank you! I'll make sure to relax and have fun.", false],
     ["Looking forward to seeing your pictures!", false],
-    ["Thanks! I'll share them with you.", false],
+    ["Thanks! I'll share them with you.", true],
     ["You too! Have a wonderful day!", false],
-    ["Thanks, take care!", false],
+    ["Thanks, take care!", true],
   ],
   // Third dialog - Person A and Person B
   [
     ["Hi", true],
-    ["How was your day?", true],
+    ["How was your day?", false],
     ["It was busy but productive. How about yours?", true],
     ["Mine was good too. I finished a project I've been working on.", true],
-    ["That's great! You've been doing amazing work lately.", true],
+    ["That's great! You've been doing amazing work lately.", false],
     ["Thank you so much! Your support means a lot to me.", true],
-    ["You deserve it. Keep up the excellent work!", true],
+    ["You deserve it. Keep up the excellent work!", false],
     ["I will. By the way, have you tried the new restaurant in town?", true],
-    ["Not yet. Is it good?", true],
+    ["Not yet. Is it good?", false],
     ["Yes, the food is delicious. We should go there together sometime.", true],
-    ["Sure, I'd love to try it. Let's plan it soon.", true],
+    ["Sure, I'd love to try it. Let's plan it soon.", false],
     ["Sounds like a plan. Looking forward to it!", true],
   ],
   // Fourth dialog - Person A and Person B
   [
     ["Hello", true],
-    ["How was your weekend?", true],
+    ["How was your weekend?", false],
     ["It was fantastic! I went on a road trip with my friends.", true],
-    ["That sounds like a lot of fun. Where did you go?", true],
+    ["That sounds like a lot of fun. Where did you go?", false],
     ["We explored the countryside and visited some scenic spots.", true],
-    ["I wish I could have joined. The pictures looked amazing.", true],
+    ["I wish I could have joined. The pictures looked amazing.", false],
     [
       "It was truly an unforgettable experience. Next time, you should come along.",
-      true
+      false
     ],
     [
       "I'll definitely join you on the next adventure. It sounds like a great time!",
       true
     ],
-    ["I'll make sure to invite you. We always have a blast together.", true],
+    ["I'll make sure to invite you. We always have a blast together.", false],
     [
       "That's what I love about our friendship. We create unforgettable memories.",
       true
     ],
     [
       "Absolutely! Our adventures are always filled with laughter and joy.",
-      true
+      false
     ],
   ],
   // Add more dialogs here
--- telegram clone/lib/user_page.dart
+++ telegram clone/lib/user_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'message_interface.dart';
 import 'data.dart';
+import 'profile.dart';
 
 class UserPage extends StatefulWidget {
   _UserPageState createState() => _UserPageState();
@@ -11,7 +12,11 @@ class _UserPageState extends State<UserPage> {
   List<Widget> user_generater() {
     List<Widget> result = [];
     for (int i = 0; i < user.length; i++) {
-      result.add(user_interface("Yohanse Mehabaw", "yohanse", i));
+      result.add(Profile(
+        name: user[i][0], 
+        image: user[i][1], 
+        id: i),
+      );
       result.add(
         SizedBox(
           width: 600,
