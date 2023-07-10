257f91ba8ecee54d9807df09a2030edcb638a9e1
yohanse <mehabawyohanse793@gmail.com>
2023-07-10 08:42:35 +0300

    flutter

--- SocialMediaAppProject/lib/main.dart
+++ SocialMediaAppProject/lib/main.dart
@@ -1,8 +1,18 @@
  import 'package:flutter/material.dart';
+ import 'package:firebase_core/firebase_core.dart';
+ import 'screens/login_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+ void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Initialize Firebase
+  runApp(Login());
 }
+
+
+
+// void main() {
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
---
+++ SocialMediaAppProject/lib/screens/login_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class  Login extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextField(),
+    );
+  }
+}
